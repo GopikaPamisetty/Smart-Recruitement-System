216a1f0d5c28ccc4bb9536a653692d13f8d17ab6
gopikapamisetty <gopikapamisetty@gmail.com>
2025-05-29 00:11:58 +0530

    modified

--- Backend/routes/jobRoute.js
+++ Backend/routes/jobRoute.js
@@ -6,7 +6,8 @@ import { getAdminJobs, getAllJobs, getJobById, postJob, updateJob, deleteJob  } 
 const router = express.Router();
 
 router.route("/post").post(isAuthenticated, postJob);
-router.route("/get").get(isAuthenticated, getAllJobs);
+// router.route("/get").get(isAuthenticated, getAllJobs);
+router.route("/get").get(getAllJobs);
 router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
 router.route("/get/:id").get(isAuthenticated, getJobById);
 router.route("/update/:id").put(isAuthenticated,updateJob);  // 🔹 Add this line
