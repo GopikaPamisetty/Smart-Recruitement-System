7cb265fd3af5ea8d25d32fa31609a2c3ea1824e1
gopikapamisetty <gopikapamisetty@gmail.com>
2025-05-28 22:12:11 +0530

    backend link

--- Backend/index.js
+++ Backend/index.js
@@ -26,10 +26,15 @@ connectDB();
 
 // Middleware
 app.use(helmet());
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN || 'https://smart-recruitement-system-live.onrender.com',
+//     credentials: true
+// }));
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'https://smart-recruitement-system-live.onrender.com',
+    origin: "https://smart-recruitement-system-live.onrender.com",
     credentials: true
-}));
+  }));
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
