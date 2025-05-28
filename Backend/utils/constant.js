5dbb525b6ed999f72aba9dde2db1939f55283f30
gopikapamisetty <gopikapamisetty@gmail.com>
2025-05-28 23:06:03 +0530

    modified constant.js

--- Backend/utils/constant.js
+++ Backend/utils/constant.js
@@ -1,1 +1,8 @@
+// export const USER_API_END_POINT = import.meta.env.VITE_API_URL + "/api/v1/user";
+// // export const USER_API_END_POINT = import.meta.env.VITE_API_URL + "/api/v1/user";
+// export const JOB_API_END_POINT = import.meta.env.VITE_API_URL + "/api/v1/job";
+// export const APPLICATION_API_END_POINT = import.meta.env.VITE_API_URL + "/api/v1/application";
 export const USER_API_END_POINT = import.meta.env.VITE_API_URL + "/api/v1/user";
+export const COMPANY_API_END_POINT = import.meta.env.VITE_API_URL + "/api/v1/company";
+export const JOB_API_END_POINT = import.meta.env.VITE_API_URL + "/api/v1/job";
+export const APPLICATION_API_END_POINT = import.meta.env.VITE_API_URL + "/api/v1/application";
